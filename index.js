bbf67288090fc3239dd31d3252d189dfcb5f1861
Eshratjahan17 <eshratjahan701@gmail.com>
2022-09-06 18:50:50 +0600

    product added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port =process.env.PORT|| 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
@@ -21,7 +21,54 @@ const client = new MongoClient(uri, {
 async function run(){
   try{
     await client.connect();
+    const productsCollection = client
+      .db("sphere-service")
+      .collection("products");
+    const usersCollection = client.db("sphere-service").collection("users");
+    //product collction
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //details
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
 
+    //users
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRATE,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
+    });
+    //product get post
+    //post
+    app.post("/addProduct", async (req, res) => {
+      const data = req.body;
+      const result = await productsCollection.insertOne(data);
+      res.send(result);
+    });
+    
   }
   finally{
 
