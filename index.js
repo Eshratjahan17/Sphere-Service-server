869559cb2014c18860412be1be1f9f453b352261
Eshratjahan17 <eshratjahan701@gmail.com>
2022-09-17 02:09:37 +0600

    delete api

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ async function run(){
       const result = await cursor.toArray();
       res.send(result);
     });
+    //delete product
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(filter);
+      res.send(result);
+    });
   }
   finally{
 
