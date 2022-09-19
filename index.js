3b534e5b90b83117059e158b230d154b23e1c2ac
Eshratjahan17 <eshratjahan701@gmail.com>
2022-09-19 11:24:19 +0600

    make admin user api

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+var jwt = require("jsonwebtoken");
 const port =process.env.PORT|| 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
-
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tczivvu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -18,6 +14,20 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+function verifyJwt(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRATE, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 async function run(){
   try{
     await client.connect();
@@ -61,6 +71,31 @@ async function run(){
         { expiresIn: "1h" }
       );
       res.send({ result, token });
+    });
+    //get Users
+    app.get("/user", verifyJwt, async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //make Admin
+    app.put("/user/admin/:email",  async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
     });
     //product get post
     //post
