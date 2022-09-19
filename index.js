ed25e26f028942fe6f39a766e66a613e79bd68ea
Eshratjahan17 <eshratjahan701@gmail.com>
2022-09-19 22:46:27 +0600

    admin dashboard

--- index.js
+++ index.js
@@ -79,17 +79,23 @@ async function run(){
       res.send(result);
     });
     //make Admin
-    app.put("/user/admin/:email",  async (req, res) => {
+    app.put("/user/admin/:email", async (req, res) => {
       const email = req.params.email;
-     
-        const filter = { email: email };
-        const updateDoc = {
-          $set: { role: "admin" },
-        };
 
-        const result = await usersCollection.updateOne(filter, updateDoc);
-        res.send(result);
-    
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    //check Admin
+    app.get("/admin/:email",verifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
     });
     //product get post
     //post
