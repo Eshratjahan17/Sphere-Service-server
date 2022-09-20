ffcc717283f84fdfd52162b9678c15e20fd2bce8
Eshratjahan17 <eshratjahan701@gmail.com>
2022-09-20 18:44:08 +0600

    all done without payment

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port =process.env.PORT|| 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
+const stripe = require("stripe")(
+"sk_test_51LjwqjJGxR1uvNyokq6lPx2Rk5L4eBotXyCfABVDM5ah9IovyyZcVFH9EmHUnPt1jG5IJ43VFMDI2pUziSJok8po00bypEeaqJ"
+);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tczivvu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -36,6 +39,7 @@ async function run(){
       .collection("products");
     const usersCollection = client.db("sphere-service").collection("users");
     const reviewCollection = client.db("sphere-service").collection("reviews");
+    const ordersCollection = client.db("sphere-service").collection("orders");
     //product collction
     app.get("/products", async (req, res) => {
       const cursor = productsCollection.find();
@@ -91,7 +95,7 @@ async function run(){
       res.send(result);
     });
     //check Admin
-    app.get("/admin/:email",verifyJwt, async (req, res) => {
+    app.get("/admin/:email", verifyJwt, async (req, res) => {
       const email = req.params.email;
       const user = await usersCollection.findOne({ email: email });
       const isAdmin = user.role === "admin";
@@ -130,6 +134,72 @@ async function run(){
       const result = await cursor.toArray();
       res.send(result);
     });
+    // update user
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const updatedUser = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: updatedUser,
+      };
+      const result = await usersCollection.updateMany(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+    //Post a review
+    app.post("/addreview", async (req, res) => {
+      const data = req.body;
+      const result = await reviewCollection.insertOne(data);
+      res.send(result);
+    });
+    //orders post
+    app.post("/order", async (req, res) => {
+      const orders = req.body;
+      const result = await ordersCollection.insertOne(orders);
+      res.send(result);
+    });
+    app.get("/order", verifyJwt, async (req, res) => {
+      const email = req.query.email;
+
+      const query = { email: email };
+      const orders = await ordersCollection.find(query).toArray();
+      res.send(orders);
+    });
+    //order by id
+    app.get("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await ordersCollection.findOne(query);
+      res.send(order);
+    });
+    app.patch("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const updatedOrder = await ordersCollection.updateOne(filter, updatedDoc);
+      const result = await paymentCollection.insertOne(payment);
+      res.send(updatedDoc);
+    });
+    //order by email
+     app.get("/myorders", async (req, res) => {
+       const email = req.query.email;
+
+       const query = { email: email };
+       const orders = await ordersCollection.find(query).toArray();
+       res.send(orders);
+     });
+     //payment intent
+      app.post('/')
   }
   finally{
 
